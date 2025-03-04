67ab509660a6dccd978ed2e969cd19666485f705
RodriDuro <00718rj@gmail.com>
2025-03-04 19:33:16 -0300

    Adicionando pasta novo desafio

---
+++ DIO/Desafio_html4/assets/js/script.js
@@ -0,0 +1,7 @@
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function scrollToGundam() {
+    document.getElementById('gundam').scrollIntoView({ behavior: 'smooth' });
+}
